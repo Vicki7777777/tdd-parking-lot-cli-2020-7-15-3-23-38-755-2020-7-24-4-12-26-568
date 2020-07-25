a74bfe8e0e08f0546a7499832fe236f4f71416eb
Vicki <458044962@qq.com>
2020-07-25 12:04:51 +0800

    [ParkingStory][Vicki]Test:should_return_wrong_when_customer_given_nullTicket test

---
+++ src/main/java/com/oocl/cultivation/Customer.java
@@ -0,0 +1,8 @@
+package com.oocl.cultivation;
+
+public class Customer {
+    public Car giveTicket(ParkingTicket parkingTicket) {
+        Car car = new Car();
+         return car;
+    }
+}
--- src/test/java/com/oocl/cultivation/test/CustomerTest.java
+++ src/test/java/com/oocl/cultivation/test/CustomerTest.java
@@ -1,8 +1,26 @@
 package com.oocl.cultivation.test;
 
 import com.oocl.cultivation.Car;
+import com.oocl.cultivation.Customer;
+import com.oocl.cultivation.ParkingBoy;
+import com.oocl.cultivation.ParkingTicket;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CustomerTest {
+
+    @Test
+    void should_return_wrong_when_customer_given_nullTicket(){
+        //give
+        Customer customer = new Customer();
+        ParkingBoy parkingBoy = new ParkingBoy();
+        ParkingTicket parkingTicket = null;
+
+        //when
+        Car car = customer.giveTicket(parkingTicket);
+        //then
+        assertEquals(null,car);
+    }
 
 }
