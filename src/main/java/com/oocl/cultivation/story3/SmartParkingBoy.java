0051e3fe1f2837303c5e27b491f62068e9a6faef
Vicki <458044962@qq.com>
2020-07-25 21:13:47 +0800

    [ParkingStory-story3][Vicki]Test:Add should_return_put_car_to_another_parkingLot_when_firstParkingLoutFull_give_multipleCar test

--- src/main/java/com/oocl/cultivation/story3/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/story3/SmartParkingBoy.java
@@ -1,12 +1,17 @@
 package com.oocl.cultivation.story3;
 
 public class SmartParkingBoy {
-    public void park(Car car) {
+    public ParkingTicket park(Car car) {
+        ParkingTicket parkingTicket = new ParkingTicket();
+        return parkingTicket;
     }
 
     public ParkingLot findParkingLot(Car car) {
+        ParkingLot parkingLot = new ParkingLot();
+        return parkingLot;
     }
 
-    public void manageParkingLot(ParkingLot parkingLot02) {
+    public void manageParkingLot(ParkingLot parkingLot) {
+
     }
 }
