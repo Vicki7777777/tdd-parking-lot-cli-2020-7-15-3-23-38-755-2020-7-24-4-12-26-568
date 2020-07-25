19022c430a240a091f0b6871015b924ca9e982b4
Vicki <458044962@qq.com>
2020-07-25 13:34:04 +0800

    [ParkingStory][Vicki]Test:Add should_return_wrong_when_capacitance_full_given_car test

--- src/test/java/com/oocl/cultivation/test/ParingLotTest.java
+++ src/test/java/com/oocl/cultivation/test/ParingLotTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ParingLotTest {
     @Test
     void should_return_wrong_when_customer_given_usedTicket(){
-        //give
+        //given
         Customer customer = new Customer();
         Car car = new Car();
         ParkingTicket parkingTicket = customer.giveCar(car);
@@ -20,5 +20,21 @@ public class ParingLotTest {
         parkingLot.checkTicket(parkingTicket);
         //then
         assertEquals(false,parkingLot.checkTicket(parkingTicket));
+    }
+    @Test
+    void should_return_wrong_when_capacitance_full_given_car(){
+        //given
+        Car car = new Car();
+        Customer customer = new Customer();
+        for(int i = 0;i<10;i++)
+        {
+            customer.giveCar(car);
+        }
+        //when
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingTicket parkingTicket = customer.giveCar(car);
+
+        //then
+        assertEquals(null,parkingTicket);
     }
 }
